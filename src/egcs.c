17ace7ad6f2d98465bfc09b5d020a8834dfebae6
Tiehuis <marctiehuis@gmail.com>
2015-05-09 14:46:11 +1200

    ElGamal encryption alterations.

--- src/egcs.c
+++ src/egcs.c
@@ -1,6 +1,9 @@
 /**
  * @file egcs.c
- * @brief implements the El-Gamal cryptoscheme
+ *
+ * The implemenation of the ElGamal cryptosystem.
+ *
+ * \todo Confirm that the functions in this scheme work as intended.
  */
 
 #include <stdio.h>
@@ -8,6 +11,24 @@
 #include "com/util.h"
 #include "hcs_rand.h"
 #include "egcs.h"
+
+egcs_public_key* egcs_init_public_key(void)
+{
+    egcs_public_key *pk = malloc(sizeof(egcs_public_key));
+    if (pk == NULL) return NULL;
+
+    mpz_inits(pk->g, pk->q, pk->h, NULL);
+    return pk;
+}
+
+egcs_private_key* egcs_init_private_key(void)
+{
+    egcs_private_key *vk = malloc(sizeof(egcs_private_key));
+    if (vk == NULL) return NULL;
+
+    mpz_inits(vk->x, vk->q, NULL);
+    return vk;
+}
 
 void egcs_generate_key_pair(egcs_public_key *pk, egcs_private_key *vk,
         hcs_rand *hr, int bits)
@@ -26,24 +47,6 @@ void egcs_generate_key_pair(egcs_public_key *pk, egcs_private_key *vk,
     mpz_set(vk->q, pk->q);
 
     mpz_clear(t);
-}
-
-egcs_public_key* egcs_init_public_key(void)
-{
-    egcs_public_key *pk = malloc(sizeof(egcs_public_key));
-    if (pk == NULL) return NULL;
-
-    mpz_inits(pk->g, pk->q, pk->h, NULL);
-    return pk;
-}
-
-egcs_private_key* egcs_init_private_key(void)
-{
-    egcs_private_key *vk = malloc(sizeof(egcs_private_key));
-    if (vk == NULL) return NULL;
-
-    mpz_inits(vk->x, vk->q, NULL);
-    return vk;
 }
 
 egcs_cipher* egcs_init_cipher(void)
@@ -73,6 +76,15 @@ void egcs_encrypt(egcs_public_key *pk, hcs_rand *hr, egcs_cipher *rop,
     mpz_clear(t);
 }
 
+void egcs_ee_mul(egcs_public_key *pk, egcs_cipher *rop, egcs_cipher *ct1,
+        egcs_cipher *ct2)
+{
+    mpz_mul(rop->c1, ct1->c1, ct2->c2);
+    //mpz_mod(rop->c1, rop->c1, pk->q);
+    mpz_mul(rop->c2, ct1->c2, ct2->c2);
+    //mpz_mod(rop->c2, rop->c2, pk->q);
+}
+
 void egcs_decrypt(egcs_private_key *vk, mpz_t rop, egcs_cipher *ct)
 {
     mpz_t t;
@@ -83,27 +95,80 @@ void egcs_decrypt(egcs_private_key *vk, mpz_t rop, egcs_cipher *ct)
     mpz_powm(rop, ct->c1, t, vk->q);
     mpz_mul(rop, rop, ct->c2);
     mpz_mod(rop, rop, vk->q);
+
+    mpz_clear(t);
 }
 
 void egcs_clear_cipher(egcs_cipher *ct)
 {
-    mpz_zeros(ct->c1, ct->c2, NULL);
+    mpz_zero(ct->c1);
+    mpz_zero(ct->c2);
 }
 
 void egcs_free_cipher(egcs_cipher *ct)
 {
-    mpz_zeros(ct->c1, ct->c2, NULL);
-    mpz_clears(ct->c1, ct->c2, NULL);
+    mpz_clear(ct->c1);
+    mpz_clear(ct->c2);
+    free(ct);
+}
+
+void egcs_clear_public_key(egcs_public_key *pk)
+{
+    mpz_zero(pk->g);
+    mpz_zero(pk->q);
+    mpz_zero(pk->h);
+}
+
+void egcs_clear_private_key(egcs_private_key *vk)
+{
+    mpz_zero(vk->x);
+    mpz_zero(vk->q);
 }
 
 void egcs_free_public_key(egcs_public_key *pk)
 {
-    mpz_clears(pk->g, pk->q, pk->h, NULL);
+    mpz_clear(pk->g);
+    mpz_clear(pk->q);
+    mpz_clear(pk->h);
     free(pk);
 }
 
 void egcs_free_private_key(egcs_private_key *vk)
 {
-    mpz_clears(vk->x, NULL);
+    mpz_clear(vk->x);
+    mpz_clear(vk->q);
     free(vk);
 }
+
+#ifdef MAIN
+int main(void)
+{
+    egcs_private_key *vk = egcs_init_private_key();
+    egcs_public_key *pk = egcs_init_public_key();
+    hcs_rand *hr = hcs_rand_init(0);
+
+    egcs_generate_key_pair(pk, vk, hr, 128);
+
+    mpz_t a, b;
+    egcs_cipher *c = egcs_init_cipher();
+    egcs_cipher *d = egcs_init_cipher();
+    mpz_init_set_ui(a, 10);
+    mpz_init_set_ui(b, 7);
+
+    egcs_encrypt(pk, hr, c, a);
+    egcs_encrypt(pk, hr, d, b);
+    //egcs_ee_mul(pk, c, c, d);
+    egcs_decrypt(vk, a, c);
+
+    gmp_printf("%Zd\n", c);
+
+    mpz_clear(a);
+    mpz_clear(b);
+    egcs_free_cipher(c);
+    egcs_free_cipher(d);
+
+    egcs_free_public_key(pk);
+    egcs_free_private_key(vk);
+    hcs_rand_free(hr);
+}
+#endif
--- src/egcs.h
+++ src/egcs.h
@@ -1,55 +1,173 @@
 /**
  * @file egcs.h
- * @brief header file for the el-gamal cryptoscheme
+ *
+ * The ElGamal scheme is a scheme which provides homormophic multiplication.
+ * It is usually used with some padding scheme similar to RSA. This
+ * implementation only provides unpadded values to exploit this property.
+ *
+ * Due to the nature of the ciphertext, this implementation provides a
+ * unique type for the ciphertext, rather than just using gmp mpz_t types.
+ * These can be used with the provided initialisation functions, and should
+ * be freed on program termination.
+ *
+ * All mpz_t values can be alises unless otherwise stated.
  */
 
 #ifndef EGCS_H
 #define EGCS_H
 
 #include <gmp.h>
+#include "hcs_rand.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Ciphertext type for use in the ElGamal scheme.
+ */
 typedef struct {
     mpz_t c1;
     mpz_t c2;
 } egcs_cipher;
 
+/**
+ * Public key for use in the ElGamal scheme.
+ */
 typedef struct {
-    mpz_t g;    // generator for group
-    mpz_t q;    // order of group
-    mpz_t h;    // g ^ x
+    mpz_t g;    /**< Generator for the cyclic group */
+    mpz_t q;    /**< Order of the cyclic group */
+    mpz_t h;    /**< g^x \in G */
 } egcs_public_key;
 
+/**
+ * Private key for use in the ElGamal scheme.
+ */
 typedef struct {
-    mpz_t x;    // random value in group g
-    mpz_t q;
+    mpz_t x;    /**< Random value in {1, ..., q-1} */
+    mpz_t q;    /**< Order of the cyclic group */
 } egcs_private_key;
 
+/**
+ * Initialise a egcs_public_key and return a pointer to the newly created
+ * structure.
+ *
+ * @return A pointer to an initialised egcs_public_key, NULL on allocation
+ *         failure
+ */
 egcs_public_key*  egcs_init_public_key(void);
 
+/**
+ * Initialise a egcs_private_key and return a pointer to the newly created
+ * structure.
+ *
+ * @return A pointer to an initialised egcs_private_key, NULL on allocation
+ *         failure
+ */
 egcs_private_key* egcs_init_private_key(void);
 
-egcs_cipher* egcs_init_cipher(void);
-
+/**
+ * Initialise a key pair with modulus size @p bits. It is required that @p pk
+ * and @p vk are initialised before calling this function. @p pk and @p vk are
+ * expected to not be NULL.
+ *
+ * In practice the @p bits value should usually be greater than 2048 to ensure
+ * sufficient security.
+ *
+ * @param pk A pointer to an initialised egcs_public_key
+ * @param vk A pointer to an initialised egcs_private_key
+ * @param hr A pointer to an initialised hcs_rand type
+ * @param bits The number of bits for the modulus of the key
+ */
 void egcs_generate_key_pair(egcs_public_key *pk, egcs_private_key *vk,
                             hcs_rand *hr, int bits);
 
+/**
+ * Initialise a egcs_cipher and return a pointer to the newly created
+ * structure.
+ *
+ * @return A pointer to an initialised egcs_cipher, NULL on allocation failure
+ */
+egcs_cipher* egcs_init_cipher(void);
+
+/**
+ * Encrypt a value @p plain1, and set @p rop to the encrypted result.
+ *
+ * @param pk A pointer to an initialised egcs_public_key
+ * @param hr A pointer to an initialised hcs_rand type
+ * @param rop egcs_cipher where the result is to be stored
+ * @param plain1 mpz_t to be encrypted
+ */
 void egcs_encrypt(egcs_public_key *pk, hcs_rand *hr, egcs_cipher *rop,
                   mpz_t plain1);
 
+/**
+ * Multiply an encrypted value @p ct1 with an encrypted value @p ct2, storing
+ * the result in @p rop.
+ *
+ * @param pk A pointer to an initialised egcs_public_key
+ * @param rop egcs_cipher where the result is to be stored
+ * @param ct1 egcs_cipher to be multiplied together
+ * @param ct2 egcs_cipher to be multiplied together
+ */
+void egcs_ee_mul(egcs_public_key *pk, egcs_cipher *rop, egcs_cipher *ct1,
+        egcs_cipher *ct2);
+
+/**
+ * Decrypt a value @p cipher1, and set @p rop to the decrypted result.
+ *
+ * @param vk A pointer to an initialised egcs_private_key
+ * @param rop mpz_t where the decrypted result is stored
+ * @param cipher1 egcs_cipher to be decrypted
+ */
 void egcs_decrypt(egcs_private_key *vk, mpz_t rop, egcs_cipher *cipher1);
 
-void egcs_ep_add(egcs_public_key *pk, mpz_t rop, mpz_t cipher1, mpz_t plain1);
-
-void egcs_ee_add(egcs_public_key *pk, mpz_t rop, mpz_t cipher1, mpz_t cipher2);
-
-void egcs_ep_mul(egcs_public_key *pk, mpz_t rop, mpz_t cipher1, mpz_t plain1);
-
-void egcs_free_public_key(egcs_public_key *pk);
-
-void egcs_free_private_key(egcs_private_key *vk);
-
+/**
+ * Zero all data related to the given egcs_cipher.
+ *
+ * @param ct A pointer to an initialised egcs_cipher
+ */
 void egcs_clear_cipher(egcs_cipher *ct);
 
+/**
+ * Free all associated memory with a given egcs_cipher.
+ *
+ * @param ct A pointer to an initialised egcs_cipher
+ */
 void egcs_free_cipher(egcs_cipher *ct);
+
+/**
+ * Zero all memory associated with a egcs_public_key. This does not free the
+ * memory, so the key is safe to use immediately after calling this.
+ *
+ * @param vk A pointer to an initialised egcs_public_key
+ */
+void egcs_clear_public_key(egcs_public_key *pk);
+
+/**
+ * Zero all memory associated with a egcs_private_key. This does not free the
+ * memory, so the key is safe to use immediately after calling this.
+ *
+ * @param vk A pointer to an initialised egcs_private_key
+ */
+void egcs_clear_private_key(egcs_private_key *vk);
+
+/**
+ * Frees a egcs_public_key and all associated memory.
+ *
+ * @param vk A pointer to an initialised egcs_public_key
+ */
+void egcs_free_public_key(egcs_public_key *pk);
+
+/**
+ * Frees a egcs_private_key and all associated memory.
+ *
+ * @param vk A pointer to an initialised egcs_private_key
+ */
+void egcs_free_private_key(egcs_private_key *vk);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

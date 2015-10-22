9c5331529d5be1088e9d89110c0129ba052e044c
tiehuis <marctiehuis@gmail.com>
2015-10-22 18:30:46 +1300

    Update benchmark programs

--- bench/pcs_decrypt.c
+++ bench/pcs_decrypt.c
@@ -1,30 +1,53 @@
 #include <gmp.h>
 #include <libhcs/pcs.h>
-#include "timing.h"
-
-#define num_runs 60
+#include "chrono.h"
 
 int main(void)
 {
+#define test_vector_size 5
+    int test_vector[test_vector_size][2] = {
+        /* num_runs, key_size */
+        { 10000, 256 },
+        { 10000, 512 },
+        { 2000, 1024 },
+        { 200,  2048 },
+        { 25,   4096 }
+    };
+
     pcs_public_key *pk = pcs_init_public_key();
     pcs_private_key *vk = pcs_init_private_key();
     hcs_random *hr = hcs_init_random();
-    pcs_generate_key_pair(pk, vk, hr, 2048);
 
-    mpz_t a, b, c;
-    mpz_inits(a, b, c, NULL);
-
-    mpz_set_ui(a, 4124124523);
-    mpz_set_ui(b, 23423523);
-    pcs_encrypt_r(pk, c, a, b);
-
-    TIME_CODE(
+    const char *core_string =
 #ifdef _OPENMP
-            "Parallel",
+            "Parallel"
 #else
-            "Single-core",
+            "Single-core"
 #endif
-        for (int i = 0; i < num_runs; ++i)
-            pcs_decrypt(vk, b, c);
-    );
+            ;
+
+    mpz_t a, b, c, d;
+    mpz_inits(a, b, c, d, NULL);
+
+    for (int i = 0; i < test_vector_size; ++i) {
+        double total = 0;
+        chrono timer;
+
+        mpz_set_ui(a, 4124124523);
+        mpz_set_ui(b, 23423508023);
+        mpz_set_ui(d, 1);
+        pcs_generate_key_pair(pk, vk, hr, test_vector[i][1]);
+        pcs_encrypt(pk, hr, a, a);
+
+        for (int j = 0; j < test_vector[i][0]; ++j) {
+            chrono_start(&timer);
+            pcs_decrypt(vk, c, a);
+            chrono_end(&timer);
+            total += chrono_get_msec(&timer);
+            pcs_ep_add(pk, a, a, d);
+        }
+
+        printf("%s: (%d): %.15f\n", core_string, test_vector[i][1],
+                total / test_vector[i][0]);
+    }
 }
--- bench/pcs_encrypt.c
+++ bench/pcs_encrypt.c
@@ -1,41 +1,52 @@
 #include <gmp.h>
-#define HCS_STATIC_SEED
 #include <libhcs/pcs.h>
-#include "timing.h"
-
-#define num_runs 1000
+#include "chrono.h"
 
 int main(void)
 {
+#define test_vector_size 5
+    int test_vector[test_vector_size][2] = {
+        /* num_runs, key_size */
+        { 10000, 256 },
+        { 10000, 512 },
+        { 2000, 1024 },
+        { 200,  2048 },
+        { 25,   4096 }
+    };
+
     pcs_public_key *pk = pcs_init_public_key();
     pcs_private_key *vk = pcs_init_private_key();
     hcs_random *hr = hcs_init_random();
-    pcs_generate_key_pair(pk, vk, hr, 2048);
 
-    mpz_t a, b, c, d;
-    mpz_inits(a, b, c, d, NULL);
-
-    mpz_set_ui(a, 4124124523);
-    mpz_set_ui(b, 23423523);
-    mpz_set_ui(d, 1);
-
-    printf("%s\n",
+    const char *core_string =
 #ifdef _OPENMP
                 "Parallel"
 #else
                 "Single-core"
 #endif
-          );
+                ;
 
-    for (int i = 0; i < 10; ++i) {
-        struct timespec s = MTIME_CODE(
-            for (int i = 0; i < num_runs; ++i)
-                pcs_encrypt(pk, hr, c, a);
-        );
+    mpz_t a, b, c, d;
+    mpz_inits(a, b, c, d, NULL);
 
-        pcs_ep_add(pk, a, a, d);
+    for (int i = 0; i < test_vector_size; ++i) {
+        double total = 0;
+        chrono timer;
 
-        printf("Elapsed:\n\t%lus %juns\n\n", s.tv_sec, s.tv_nsec);\
+        mpz_set_ui(a, 4124124523);
+        mpz_set_ui(b, 23423508023);
+        mpz_set_ui(d, 1);
+        pcs_generate_key_pair(pk, vk, hr, test_vector[i][1]);
+
+        for (int j = 0; j < test_vector[i][0]; ++j) {
+            chrono_start(&timer);
+            pcs_encrypt(pk, hr, c, a);
+            chrono_end(&timer);
+            total += chrono_get_msec(&timer);
+            pcs_ep_add(pk, a, a, d);
+        }
+
+        printf("%s: (%d): %.15f\n", core_string, test_vector[i][1],
+                total / test_vector[i][0]);
     }
-
 }
